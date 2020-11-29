1b8400515a06ee1cd2178245be7983da91c9461c
Navneet-Suresh <5709753+Navneet-Suresh@users.noreply.github.com>
2020-11-30 03:55:48 +0530

    Update deploy script
    Signed-off-by: Navneet-Suresh <5709753+Navneet-Suresh@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Navneet-Suresh/notes.git master:gh-pages
-
+git push -f git@github.com:Navneet-Suresh/notes.git gh-pages
 cd -
