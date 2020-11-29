df5c3b3771a6c29db64dbeb8ce2969cdde0d2810
Navneet-Suresh <5709753+Navneet-Suresh@users.noreply.github.com>
2020-11-30 04:47:09 +0530

    Update deploy script and fix it
    Signed-off-by: Navneet-Suresh <5709753+Navneet-Suresh@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Navneet-Suresh/notes.git gh-pages
+git push -f git@github.com:Navneet-Suresh/notes.git master:gh-pages
+
 cd -
